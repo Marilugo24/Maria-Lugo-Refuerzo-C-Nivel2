2032f3ed0e768ae9d33dc6317e4063f781f5e091
Maria Lugo <mariafernanda.lugobolivar@gmail.com>
2024-06-06 12:53:16 -0400

    Terminado

---
+++ Contador_Palabras/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Por favor ingresa un texto: ");
+string TextoUsuario = Console.ReadLine();
+
+string[] Texto = TextoUsuario.Split(new char[] { ' ' });
+
+foreach (string texto in Texto)
+{
+    Console.WriteLine(texto);
+}
+
+Dictionary<string, int> FrecuenciaPalabras = new Dictionary<string, int>();
+
+foreach (string texto in Texto)
+{
+    if (FrecuenciaPalabras.ContainsKey(texto))
+    {
+        FrecuenciaPalabras[texto]++;
+    }
+    else
+    {
+        FrecuenciaPalabras[texto] = 1;
+    }
+}
+
+foreach (KeyValuePair<string, int> entrada in FrecuenciaPalabras)
+{
+    Console.WriteLine("Palabra: " + entrada.Key + ", Frecuencia: " + entrada.Value);
+}
+
